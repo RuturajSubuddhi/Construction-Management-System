3b8b5ce9656b104d225ec0108759e09e30f925df
Ruturaj Subuddhi <151379867+RuturajSubuddhi@users.noreply.github.com>
2023-11-18 19:23:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//  const formOpenBtn = document.querySelector("#form-open");
+//  home= document.querySelector(".home");
+//  formContainer = document.querySelector("form_containeer");
+//  formCloseBtn = document.querySelector(".form_close");
+//  formCloserBtn = document.querySelector(".form_close");
+//  addNewProperty = document.querySelector("#add_new_property");
+//  formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+//  formCloseBtn.addEventListener("click", () => home.remove("show"))
